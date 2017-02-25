db3d1515ba35d9479173b5f0e0352b62214507d9
SMukhin <stnslmukhin@gmail.com>
2017-02-26 02:24:50 +0300

    доделки от 26.02

--- source/js/app.js
+++ source/js/app.js
@@ -1,21 +1,37 @@
-// (function() {
-//   'use strict';
+'use strict';
+var map;
+var mapStyle = [{
+    "featureType": "administrative",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#444444"}]
+}, {"featureType": "landscape", "elementType": "all", "stylers": [{"color": "#f2f2f2"}]}, {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [{"visibility": "off"}]
+}, {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [{"saturation": -100}, {"lightness": 45}]
+}, {
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [{"visibility": "simplified"}]
+}, {
+    "featureType": "road.arterial",
+    "elementType": "labels.icon",
+    "stylers": [{"visibility": "off"}]
+}, {"featureType": "transit", "elementType": "all", "stylers": [{"visibility": "off"}]}, {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [{"color": "#00bfa5"}, {"visibility": "on"}]
+}];
 
-//   setTimeout(function() {
-//     document.querySelector('.greating_picture').classList.add('m--show');
-//   }, 1000);
-// })();
+function initMap() {
 
-import {square, MyClass} from './module';
-
-// 25
-console.log(square(10));
-
-var cred = {
-    name: 'Станислав Мухин',
-    enrollmentNo: 11115078
+    map = new google.maps.Map(document.getElementById('google-map'), {
+        center: {lat: 55.67337, lng: 37.28185},
+        zoom: 8
+    });
+    map.setOptions({styles:mapStyle})
 }
-
-var x = new MyClass(cred);
-
-console.log(x.getName());
+var asd = 'asdasdasd';
